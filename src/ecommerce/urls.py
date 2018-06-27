5ce8ef62130ae77023b97a0fccda83ac4d64d2f7
flex2016 <reloded2004@hotmail.com>
2018-06-27 12:53:03 -0400

    moved files around

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -31,6 +31,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
